3406b703609d8ebc33c59c70a95d341029d9d9b8
Remco Bloemen <remco@qubis.nl>
2012-05-07 13:43:44 +0200

    Validator: More debug output

--- src/Passes/Validator.cpp
+++ src/Passes/Validator.cpp
@@ -1,5 +1,6 @@
 #include "Passes/Validator.h"
 #include <Parser/IdentifierProperty.h>
+#include <Parser/SourceProperty.h>
 
 Validator::Validator(DataFlowGraph* program)
 : _program(program)
@@ -104,26 +105,22 @@ void Validator::visit(int i)
 
 void Validator::visit(Node* node)
 {
-	if(node->type() == NodeType::Call)
-		if(node->in(0)->has(PropertyType::Identifier))
-			wcerr << node->in(0)->get<IdentifierProperty>().value();
-		else
-			wcerr << node;
-	else if (node->type() == NodeType::Closure)
-		if(node->out(0)->has(PropertyType::Identifier))
-			wcerr << node->out(0)->get<IdentifierProperty>().value();
-		else
-			wcerr << node;
+	wcerr << node;
 }
 
 void Validator::print()
 {
+	wcerr << "Preorder numbers: " << endl;
 	for(int i = 0; i < _program->nodes().size(); ++i) {
+		wcerr << "\t";
 		visit(_program->nodes()[i]);
 		wcerr << "\t" << _preorder[i] << "\t" << _component[i] << endl;
 	}
+	wcerr << endl;
+	wcerr << "Strongly connected components: " << endl;
 	for(int c = 1; c <= _componentCounter; ++c)
 	{
+		wcerr << "\t[";
 		vector<Node> group;
 		for(int i = 0; i < _program->nodes().size(); ++i) {
 			if(_component[i] == c) {
@@ -131,7 +128,8 @@ void Validator::print()
 				wcerr << " ";
 			}
 		}
-		wcerr << endl;
+		wcerr << "]" << endl;
 	}
+	wcerr << endl;
 }
 

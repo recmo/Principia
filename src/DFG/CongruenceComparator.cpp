5e2fe22fb3f0bafa2968a165d24f4850379af9a1
Remco Bloemen <remco@coblue.eu>
2015-11-18 00:52:06 +0100

    Fixed CongruenceComparator

--- src/DFG/CongruenceComparator.cpp
+++ src/DFG/CongruenceComparator.cpp
@@ -2,80 +2,34 @@
 #include <DFG/Node.h>
 #include <DFG/ConstantProperty.h>
 #include <Unicode/exceptions.h>
+#include <Utilities/exceptions.h>
 #include <iostream>
 using std::wcerr;
 using std::endl;
 
 #define debug false
 
-/*
-
-bool CongruenceComparator::compareEdges(
-	std::shared_ptr<Edge> left, std::shared_ptr<Edge> right)
+bool CongruenceComparator::compareNode(const Node& left, const Node& right)
 {
 	if(debug)
 		wcerr << L"Comparing " << left << L" ≡₁ " << right << endl;
 	
-	if(left == nullptr || right == nullptr)
-		return false;
-	if(left == right)
+	// Check trivial identity
+	if(&left == &right)
 		return true;
 	
-	// Apply any edge mapping
-	auto leftMap = _connectLeft.find(left);
-	if(leftMap != _connectLeft.end())
-		left = leftMap->second;
-	auto rightMap = _connectRight.find(right);
-	if(rightMap != _connectRight.end())
-		right = rightMap->second;
-	
-	// Sourceless edges (for example constants)
-	if(left->source() == nullptr || right->source() == nullptr ) {
-		if(left->source() != right->source())
-			return false;
-		if(left->has<ConstantProperty>() && right->has<ConstantProperty>()) {
-			return compareValues(
-				left->get<ConstantProperty>().value(),
-				right->get<ConstantProperty>().value());
-		}
-		throw runtime_error(L"Unimplemented");
-	}
-	
-	// Same output in the defining node
-	if(left->index() != right->index())
+	// Check basic shape
+	if(left.type() != right.type())
 		return false;
-	
-	// Recursion pairs
-	if(contains(_recursionPairs, std::make_pair(left, right)))
-		return true;
-	
-	// Compare the defining nodes
-	return compareNodes(left->source(), right->source());
-}
-
-bool CongruenceComparator::compareNodes(
-	std::shared_ptr<Node> left, std::shared_ptr<Node> right)
-{
-	if(debug)
-		wcerr << L"Comparing " << left << L" ≡₂ " << right << endl;
-	
-	if(left == nullptr|| right == nullptr)
+	if(left.outArity() != right.outArity())
 		return false;
-	if(left == right)
-		return true;
-	
-	// Equivalence of type and arity
-	if(left->type() != right->type())
-		return false;
-	if(left->outArity() != right->outArity())
-		return false;
-	if(left->inArity() != right->inArity())
+	if(left.inArity() != right.inArity())
 		return false;
 	
 	// Calls are structurally equal iff all their inputs are equal
-	if(left->type() == Node::Call) {
-		for(uint i = 0; i < left->inArity(); ++i) {
-			if(!compareEdges(left->in(i), right->in(i)))
+	if(left.type() == Node::Call) {
+		for(uint i = 0; i < left.inArity(); ++i) {
+			if(!compareIn(left.in(i), right.in(i)))
 				return false;
 		}
 		return true;
@@ -83,38 +37,91 @@ bool CongruenceComparator::compareNodes(
 	
 	// Closures are equal iff their incoming edges are equal under the
 	// assumption that their outgoing edges are equal.
-	if(left->type() == Node::Closure) {
+	if(left.type() == Node::Closure) {
 		
 		// Push the equality of the outgoing edges on the stack
-		for(uint i = 0; i < left->outArity(); ++i) {
+		for(uint i = 0; i < left.outArity(); ++i) {
 			if(debug) {
-				wcerr << "Pushing " << left->out(i);
-				wcerr << " ≡ " << right->out(i) << endl;
+				wcerr << "Pushing " << left.out(i);
+				wcerr << " ≡ " << right.out(i) << endl;
 			}
-			_recursionPairs.push_back(std::make_pair(left->out(i), right->out(i)));
+			_recursionPairs.push_back(std::make_pair(
+				left.out(i).shared_from_this(),
+				right.out(i).shared_from_this()));
 		}
 		
 		// Compare the output of the closure
 		bool equal = true;
-		for(uint i = 0; i < left->inArity(); ++i) {
-			if(!compareEdges(left->in(i), right->in(i))) {
+		for(uint i = 0; i < left.inArity(); ++i) {
+			if(!compareIn(left.in(i), right.in(i))) {
 				equal = false;
 				break;
 			}
 		}
 		
 		// Pop the equality of the parameters
-		for(uint i = 0; i < left->outArity(); ++i) {
+		for(uint i = 0; i < left.outArity(); ++i) {
 			if(debug)
-				wcerr << "Poping " << left->out(i) << " ≡ " << right->out(i) << endl;
-			const auto pair = std::make_pair(left->out(i), right->out(i));
+				wcerr << "Popping " << left.out(i) << " ≡ " << right.out(i) << endl;
+			const auto pair = std::make_pair(
+				left.out(i).shared_from_this(),
+				right.out(i).shared_from_this());
 			assert(_recursionPairs.back() == pair);
 			_recursionPairs.pop_back();
 		}
 		return equal;
 	}
+
+	// Invalid node type
+	throw invalid_enum{};
+}
+
+bool CongruenceComparator::compareIn(const InPort& left, const InPort& right)
+{
+	// Apply any edge mapping
+	/* TODO
+	auto leftMap = _connectLeft.find(left);
+	if(leftMap != _connectLeft.end())
+		left = leftMap->second;
+	auto rightMap = _connectRight.find(right);
+	if(rightMap != _connectRight.end())
+		right = rightMap->second;
+	*/
+
+	// Sourceless edges (for example constants)
+	std::shared_ptr<OutPort> left_source = left.source();
+	std::shared_ptr<OutPort> right_source = right.source();
+	if(left_source == nullptr || right_source == nullptr) {
+		if(left_source != right_source)
+			return false;
+		if(left.has<ConstantProperty>() && right.has<ConstantProperty>()) {
+			return compareValues(
+				left.get<ConstantProperty>().value(),
+				right.get<ConstantProperty>().value());
+		}
+		throw unimplemented{};
+	}
 	
-	throw L"Unimplemented node type";
+	// Compare the sources
+	assert(left_source != nullptr);
+	assert(right_source != nullptr);
+	const bool equal = compareOut(*left_source, *right_source);
+	return equal;
+}
+
+bool CongruenceComparator::compareOut(const OutPort& left, const OutPort& right)
+{
+	// Recursion pairs
+	const OutPair pair{left.shared_from_this(), right.shared_from_this()};
+	if(contains(_recursionPairs, pair))
+		return true;
+	
+	// Same output in the defining node
+	if(left.index() != right.index())
+		return false;
+	
+	// Compare the defining nodes
+	return compareNode(left.parent(), right.parent());
 }
 
 bool CongruenceComparator::compareValues(const Value& left, const Value& right)
@@ -123,5 +130,3 @@ bool CongruenceComparator::compareValues(const Value& left, const Value& right)
 		wcerr << L"Comparing " << left << L" ≡₃ " << right << endl;
 	return left == right;
 }
-
-*/
--- src/DFG/CongruenceComparator.h
+++ src/DFG/CongruenceComparator.h
@@ -1,11 +1,10 @@
 #pragma once
+#include <DFG/Node.h>
 #include <vector>
 #include <map>
 #include <memory>
 class Node;
 class Value;
-
-/*
 
 /// Compare parts of the data flow graph for congruence
 /// This captures the notion structural equivalence, isomorphism, intensional
@@ -13,20 +12,21 @@ class Value;
 class CongruenceComparator
 {
 public:
-	typedef std::map<std::shared_ptr<OutPort>, std::shared_ptr<OutPort>> Connections;
+	typedef std::map<std::shared_ptr<OutPort>,
+		std::shared_ptr<OutPort>> Connections;
 	
 	void connectLeft(const Connections& connections) { _connectLeft = connections; }
 	void connectRight(const Connections& connections) { _connectRight = connections; }
 	
-	bool compareEdges(std::shared_ptr<Edge> left, std::shared_ptr<Edge> right);
-	bool compareNodes(std::shared_ptr<Node> left, std::shared_ptr<Node> right);
+	bool compareNode(const Node& left, const Node& right);
+	bool compareIn(const InPort& left, const InPort& right);
+	bool compareOut(const OutPort& left, const OutPort& right);
 	bool compareValues(const Value& left, const Value& right);
 	
 protected:
 	Connections _connectLeft;
 	Connections _connectRight;
-	typedef std::pair<std::shared_ptr<Edge>, std::shared_ptr<Edge>> EdgePair;
-	std::vector<EdgePair> _recursionPairs;
+	typedef std::pair<std::shared_ptr<const OutPort>,
+		std::shared_ptr<const OutPort>> OutPair;
+	std::vector<OutPair> _recursionPairs;
 };
-
-*/
